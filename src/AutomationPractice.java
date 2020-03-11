3638ad63106c5ca302b26ae3524b055b4f9b2dc6
paolatellezg <paolatellezg@gmail.com>
2020-03-11 11:05:56 -0500

    Refactor of the code

--- src/AutomationPractice.java
+++ src/AutomationPractice.java
@@ -4,59 +4,91 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.*;
 import java.util.concurrent.TimeUnit;
 
 public class AutomationPractice {
-    public static void main(String[] args) throws Exception{
 
+    public static WebDriver configurationDriver(String page){
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.navigate().to("http://automationpractice.com/index.php?");
+       // WebDriverWait=new WebDriverWait(driver,2);
+        System.out.println("Ir a la página");
+        driver.navigate().to(page);
         driver.manage().window().maximize();
-       JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-       //WebElement product= driver.findElement(By.xpath("//ul[@id='homefeatured']//div[@class='product-container'][1]"));
-        WebElement product= driver.findElement(By.xpath("//ul[@id='homefeatured']//li[contains(@class,'ajax_block_product')][4]"));
-        Actions over=new Actions(driver);
-        over.moveToElement(product).perform();
-        WebElement locator = product.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
-        locator.click();
-        Thread.sleep(5000);
-        driver.close();
+        return driver;
+    }
+
+    class Product{
+
+        //Properties
+       private boolean mouseOverStatus;
+        private WebElement btnAddToCart, btnQuickView, btnMore, imgProductPhoto;
+        private float value;
+        private String productName, productCategory;
 
 
+        Product(){
+
+        }
 
 
-//        WebElement we= WebDriver.findElement(By.xpath("//a[@title='Add to cart']")).click().build().perform();
-        //product.click();
+        //Actions or methods
+       public static SelectProduct(int productIndex){
+
+       }
+        public static addProduct(int productIndex){
+
+        }
+    }
+    public static void main(String[] args) throws Exception{
+
+    WebDriver driver= configurationDriver("http://automationpractice.com/index.php?");
+    Actions over=new Actions(driver);
+
+    WebElement popularTab=driver.findElement(By.xpath("//*[@class=\"homefeatured\"]"));
+    popularTab.click();
+    Thread.sleep(3000);
+    WebElement productPop= driver.findElement(By.xpath("//ul[@id='homefeatured']//li[contains(@class,'ajax_block_product')][4]"));
+    over.moveToElement(productPop).perform();
+    productPop.click();
+    WebElement addToCartButton = productPop.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
+    addToCartButton.click();
+    Thread.sleep(3000);
+    WebElement btnContinueShopping=driver.findElement(By.xpath("//div[@id='layer_cart']//div[@class='button-container']//span[contains(@class,'continue')]"));
+    btnContinueShopping.click();
+    Thread.sleep(3000);
 
 
-       /* Actions builder = new Actions(driver);
-builder.moveToElement(hoverElement).perform();
-By locator = By.id("clickElementID");
-driver.click(locator);
+    WebElement bestSellerTab=driver.findElement(By.xpath("//ul[@id='home-page-tabs']//li[2]//a[contains(@class,'blockbestsellers')]"));
+    bestSellerTab.click();
+    Thread.sleep(3000);
+    WebElement productBS=driver.findElement(By.xpath("//ul[@id='blockbestsellers']//li[contains(@class,'ajax_block_product')][3]"));
+    productBS.click();
+    over.moveToElement(productBS).perform();
+    WebElement bestSellerAddCart=productBS.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
+    bestSellerAddCart.click();
+    Thread.sleep(3000);
+    btnContinueShopping=driver.findElement(By.xpath("//div[@id='layer_cart']//div[@class='button-container']//span[contains(@class,'continue')]"));
+    btnContinueShopping.click();
+    Thread.sleep(3000);
 
-       Actions builder = new Actions(driver);
-builder.moveToElement(hoverElement).perform();
-builder.moveToElement(clickElement).click().perform();
+    WebElement bestSellerTab=driver.findElement(By.xpath("//ul[@id='home-page-tabs']//li[2]//a[contains(@class,'blockbestsellers')]"));
+    bestSellerTab.click();
+    Thread.sleep(3000);
+    WebElement productBS=driver.findElement(By.xpath("//ul[@id='blockbestsellers']//li[contains(@class,'ajax_block_product')][7]"));
+    productBS.click();
+    over.moveToElement(productBS).perform();
+    WebElement bestSellerAddCart=productBS.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
+    bestSellerAddCart.click();
+    Thread.sleep(3000);
+    btnContinueShopping=driver.findElement(By.xpath("//div[@id='layer_cart']//div[@class='button-container']//span[contains(@class,'continue')]"));
+    btnContinueShopping.click();
+    Thread.sleep(3000);
 
-
-       Actions action = new Actions(webdriver);
-        WebElement we = webdriver.findElement(By.xpath("html/body/div[13]/ul/li[4]/a"));
-        action.moveToElement(we).moveToElement(webdriver.findElement(By.xpath("/expression-here"))).click().build().perform();
-
-
-        product.over
-    //a[@title='Add to cart']
-        product.click();
-        WebElement addToCartButton= driver.findElement(By.id("add-to-cart-button"));
-        addToCartButton.click();
-        WebDriver.Timeouts timeouts = driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MICROSECONDS);
-        driver.close();*/
-
-
+  //driver.close();
 
     }
 
